567a165ec232055eedd1cb5212e2338914de00b0
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2019-03-08 12:20:13 +0500

    Added requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='requests-filecache',
-    version='0.0.1',
+    version='0.0.2',
     packages=['requests_filecache'],
     url='https://github.com/strizhechenko/requests-filecache',
     license='MIT',
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
-    description='Saves fetched HTML into files in current directory.'
+    description='Saves fetched HTML into files in current directory.',
+    install_requires=['requests']
 )
